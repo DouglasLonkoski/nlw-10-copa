abe5d80a1bb99298cd4f31af4456cd9bc772bf6a
Douglas Lonkoski <douglaslonkoski6@hotmail.com>
2022-11-06 01:21:02 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function createGame(player1, hour, player2){
+    return`
+    <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+    `
+}
+
+let delay= -0.4;
+function createcard(date, day, games){
+    delay = delay + 0.4;
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+        <h2>${date} <span>${day}</span></h2>
+        <ul>
+        ${games}
+        </ul>
+    </div>
+    `
+}
+
+document.querySelector('#cards').innerHTML = 
+    createcard("24/11", "quinta", 
+        createGame("brazil", "16:00", "serbia")+
+            createGame("brazil", "13:00", "switzerland")+
+                createGame("brazil", "16:00", "camerron"))+ 
+    createcard("28/11", "segunda", 
+        createGame("brazil", "16:00", "serbia")+
+            createGame("brazil", "13:00", "switzerland")+
+                createGame("brazil", "16:00", "camerron"))+ 
+    createcard("02/12", "sexta", 
+        createGame("brazil", "16:00", "serbia")+
+            createGame("brazil", "13:00", "switzerland")+
+                createGame("brazil", "16:00", "camerron")) 
